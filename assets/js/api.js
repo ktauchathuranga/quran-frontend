55d7f6701a7c16d1023d03fd011fba0793a40e5e
Ashen Chathuranga <ktauchathuranga@gmail.com>
2025-01-13 21:52:54 +0530

    chage server to test locally

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-const apiServer = "http://localhost:8080";
+const apiServer = "http://192.168.254.155:8080";
 
 // ngrok static URL: https://badly-evident-chamois.ngrok-free.app
 // ngrok command: ngrok http --url badly-evident-chamois.ngrok-free.app 8080
