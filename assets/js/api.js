49361c378d2d0bd76e1d453ae95a8532fb9f2f4d
Ashen Chathuranga <ktauchathuranga@gmail.com>
2025-02-06 01:26:38 +0530

    api url updated
    using v1

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-const apiServer = "https://quran.x10.mx";
+const apiServer = "https://quran.x10.mx/v1/api";
 
 // local testing URL: http://192.168.254.155:8080
 // ngrok static URL: https://badly-evident-chamois.ngrok-free.app
