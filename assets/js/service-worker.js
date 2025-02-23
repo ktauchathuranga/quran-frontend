c58e07cf2b16e9db1221cd682ed883f7f09ed66a
Ashen Chathuranga <ktauchathuranga@gmail.com>
2025-02-23 19:19:09 +0530

    cache update v2

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'website-cache-v1';
+const CACHE_NAME = 'website-cache-v2';
 const ASSETS_TO_CACHE = [
   '/', '/index.html', '/manifest.json', '/robot.txt', '/sitemap.xml', '/version.json',
   // CSS
